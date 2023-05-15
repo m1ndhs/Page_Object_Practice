2117d0b753881b0ab0f76dfda372b6dd1e99314f
m1ndhs <mndlower@gmail.com>
2023-05-15 20:58:16 +0200

    Fix for test_message.

--- test_product_page.py
+++ test_product_page.py
@@ -74,11 +74,10 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_presented_success_message()
 @pytest.mark.need_review
-@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     time.sleep(1)
-    page.should_see_as_dissapearing_message()
+    page.should_not_be_presented_success_message()
